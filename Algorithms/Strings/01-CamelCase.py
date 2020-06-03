a4aa463817c4c2812cd66beb3dc87b36e72e7842
CharuniKaushalya <snkaushi@gmail.com>
2020-06-03 21:34:14 +0530

    camel case

---
+++ Algorithms/Strings/01-CamelCase.py
@@ -0,0 +1,19 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the camelcase function below.
+def camelcase(s):
+    splitted = re.sub('([A-Z][a-z]+)', r' \1', re.sub('([A-Z]+)', r' \1', s)).split()
+    return len(splitted)
+
+if __name__ == '__main__':
+     s = input()
+
+    result = camelcase(s)
+
+    print(str(result))
