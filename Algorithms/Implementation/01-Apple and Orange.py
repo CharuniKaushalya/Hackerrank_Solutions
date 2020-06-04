eb1867615d33d76acc66c4663cfbdec78e8d2557
CharuniKaushalya <snkaushi@gmail.com>
2020-06-04 21:12:10 +0530

    apple and orange

---
+++ Algorithms/Implementation/01-Apple and Orange.py
@@ -0,0 +1,45 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the countApplesAndOranges function below.
+def countApplesAndOranges(s, t, a, b, m, n, apples, oranges):
+    counta = 0
+    countb = 0
+    for i in range(m):
+        if apples[i]+a in range(s,t+1):
+            counta = counta+1
+    for j in range(n):
+        if oranges[j]+b in range(s,t+1):
+            countb = countb +1    
+    print(counta)
+    print(countb)      
+
+if __name__ == '__main__':
+    st = input().split()
+
+    s = int(st[0])
+
+    t = int(st[1])
+
+    ab = input().split()
+
+    a = int(ab[0])
+
+    b = int(ab[1])
+
+    mn = input().split()
+
+    m = int(mn[0])
+
+    n = int(mn[1])
+
+    apples = list(map(int, input().rstrip().split()))
+
+    oranges = list(map(int, input().rstrip().split()))
+
+    countApplesAndOranges(s, t, a, b, m,n, apples, oranges)
