87bdf2d4f35cc025cb1765864238fc2f7e7b700b
CharuniKaushalya <snkaushi@gmail.com>
2020-06-04 21:14:28 +0530

    optimized

--- Algorithms/Implementation/01-Apple and Orange.py
+++ Algorithms/Implementation/01-Apple and Orange.py
@@ -5,21 +5,12 @@ import os
 import random
 import re
 import sys
-# print(sum(s <= a + i <= t for i in apple))
-# print(sum(s <= b + i <= t for i in orange))
+
 
 # Complete the countApplesAndOranges function below.
 def countApplesAndOranges(s, t, a, b, m, n, apples, oranges):
-    counta = 0
-    countb = 0
-    for i in apples:
-        if i+a in range(s,t+1):
-            counta = counta+1
-    for j in oranges:
-        if j+b in range(s,t+1):
-            countb = countb +1    
-    print(counta)
-    print(countb)      
+    print(sum(s <= a + i <= t for i in apple))
+    print(sum(s <= b + i <= t for i in orange))    
 
 if __name__ == '__main__':
     st = input().split()
