0fbfe6af089030b5d667197bf8d75dc40f3f2aff
CharuniKaushalya <snkaushi@gmail.com>
2020-06-05 19:17:16 +0530

    kangaroo

---
+++ Algorithms/Implementation/02-Kangaroo.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the kangaroo function below.
+def kangaroo(x1, v1, x2, v2):
+    a = list(range(x1,10000,v1))
+    b = list(range(x2,10000,v2))
+    if len([i for i, j in zip(a, b) if i == j]) > 0 :
+        print("YES")
+    else:
+        print("NO")
+
+if __name__ == '__main__':
+
+    x1V1X2V2 = input().split()
+
+    x1 = int(x1V1X2V2[0])
+
+    v1 = int(x1V1X2V2[1])
+
+    x2 = int(x1V1X2V2[2])
+
+    v2 = int(x1V1X2V2[3])
+
+    result = kangaroo(x1, v1, x2, v2)
