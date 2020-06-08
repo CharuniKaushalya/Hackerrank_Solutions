a2e472af132544e3e9aa27c635b68d6919939746
CharuniKaushalya <snkaushi@gmail.com>
2020-06-08 22:09:33 +0530

    stylished

--- Algorithms/Implementation/06-Migratory Birds.py
+++ Algorithms/Implementation/06-Migratory Birds.py
@@ -1,21 +1,18 @@
 #!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
+"""
+Solve https://www.hackerrank.com/challenges/migratory-birds/problem
+"""
 
 # Complete the migratoryBirds function below.
-def migratoryBirds(List):
-    return max(set(List), key = List.count) 
+def migratory_birds(arr):
+    return max(set(arr), key=arr.count) 
 
 if __name__ == '__main__':
 
     arr_count = int(input().strip())
 
-    arr = list(map(int, input().rstrip().split()))
+    bird_arr = list(map(int, input().rstrip().split()))
 
-    result = migratoryBirds(arr)
+    result = migratory_birds(bird_arr)
 
     print(result)
