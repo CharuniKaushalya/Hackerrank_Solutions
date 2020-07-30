e65e481ea4f949ce84333afda0e1cfed57816455
Charuni Kaushalya <snkaushi@gmail.com>
2020-07-30 15:22:39 +0530

    Create 18-Beautiful Days at the Movies.py

---
+++ Algorithms/Implementation/18-Beautiful Days at the Movies.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the beautifulDays function below.
+def beautifulDays(i, j, k):
+    count = 0
+    for i in range(i,j+1):
+        if(abs(i-int(str(i)[::-1]))%k  == 0):
+            count +=1
+    return count
+
+if __name__ == '__main__':
+    ijk = input().split()
+
+    i = int(ijk[0])
+
+    j = int(ijk[1])
+
+    k = int(ijk[2])
+
+    result = beautifulDays(i, j, k)
+
+    print(result)
